f334379af226aec4c5279d0150813530b7610d58
Kieran Herr <kieranherr@gmail.com>
2020-04-23 12:30:29 -0500

    Worked on the Identity PDF. I somehow didn't do it right and don't have the Login, Logout, or Register files in the Areas folder.

---
+++ TrashCollector/Models/Model1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Model1
+    {
+        [Key]
+        public string address { get; set;}
+    }
+}
